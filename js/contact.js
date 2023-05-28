98771edc038b93d4f701d96c110aac1ea1da9b0b
ertugruldugencioglu <ertugruldugenci@gmail.com>
2023-05-28 14:30:36 +0300

    home page is over

---
+++ js/contact.js
@@ -0,0 +1,7 @@
+function moveContacts(index) {
+    if (index < 0 || index >= document.querySelectorAll(".carousel-item").length) {
+      return;
+    }
+    document.querySelector(".carousel").style.transform = `translateY(-${index * 25}%)`;
+}
+//*******************************/
