de7d47edaf4db1a3790ae26e22e934909515d669
ertugruldugencioglu <ertugruldugenci@gmail.com>
2023-05-30 19:00:28 +0300

    login possible

--- js/logIn.js
+++ js/logIn.js
@@ -4,7 +4,7 @@ function logIn(){
     var eMail = document.getElementById("e-mail").value;
     var password = document.getElementById("password").value;
     if (eMail == userData.eMail && password == userData.password){
-        window.location.href="../main-showcase.html";
+        window.location.href="./main-showcase.html";
     } else {
         alert("Böyle bir kullanıcı bulunamadı!")
     }
