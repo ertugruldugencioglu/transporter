b5f1c4a56abd6ce2fb2815e0d5f56cadae850aea
ertugruldugencioglu <ertugruldugenci@gmail.com>
2023-06-01 17:50:01 +0300

    add an advert button

--- js/general.js
+++ js/general.js
@@ -1,47 +1,58 @@
 var openLinks = document.querySelectorAll('.open-link');
 var favoriteButton = document.querySelectorAll('.add-favorite');
-var modal = document.getElementById('modal');
-var closeButton = document.getElementById('close-button');
-var div = document.getElementById("acoount-settings");
+var advertModal = document.getElementById('advert-modal');
+var advertCloseButton = document.getElementById('advert-modal-close-button');
+var addAdvertModal = document.getElementById('add-advert-modal');
+var addAdvertCloseButton = document.getElementById('add-advert-modal-close-button');
+var user = document.getElementById("acoount-settings");
 var addButton = document.getElementById("add-advert");
 var userData = localStorage.getItem("userData");
 var parsedUserData = JSON.parse(userData);
+var buy = document.getElementById("buy-button");
 
 if(parsedUserData.sector == "broker"){
+    buy.style.display = "none"
     addButton.style.display = 'flex'
     addButton.style.alignItems = 'center'
     addButton.style.gap = '8px'
+    
 } else {
-    addButton.style.display = 'hidden'
+    addButton.style.display = 'none'
 }
 openLinks.forEach(openLink => {
   openLink.addEventListener('click', (event) => {
     event.preventDefault();
-    modal.style.display = 'block';
+    advertModal.style.display = 'block';
   });
 });
-closeButton.addEventListener('click', () => {
-  modal.style.display = 'none';
+advertCloseButton.addEventListener('click', () => {
+  advertModal.style.display = 'none';
+});
+function addAdvert(){
+  addAdvertModal.style.display = 'block';
+}
+addAdvertCloseButton.addEventListener('click', () => {
+  addAdvertModal.style.display = 'none';
 });
 function toggleDiv() {
-  if (div.style.display === "block") {
-    div.style.display = "none";
+  if (user.style.display === "block") {
+    user.style.display = "none";
   } else {
-    div.style.display = "block";
+    user.style.display = "block";
   }
 }
 function logOut(){
   localStorage.clear();
-  div.style.display = "none";
+  user.style.display = "none";
   window.location.href="./index.html"
 }
 favoriteButton.forEach(addFavorites => {
   addFavorites.addEventListener('click', () => {
-    var resim = document.querySelector('.button-img');
-    if(resim.src == "file:///C:/Users/ertug/OneDrive/Masa%C3%BCst%C3%BC/transporter/transporter/img/heart.png"){
-      resim.src = 'img/love.png';
+    var img = document.querySelector('.button-img');
+    if(img.src == "file:///C:/Users/ertug/OneDrive/Masa%C3%BCst%C3%BC/transporter/transporter/img/heart.png"){
+      img.src = 'img/love.png';
     } else {
-      resim.src = 'img/heart.png'
+      img.src = 'img/heart.png'
     }
   });
 });
